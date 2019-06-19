fffef4c4b1e7f4b2319e82693888e6e5b2854636
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-06-20 01:50:23 +0530

    init student

---
+++ student.java
@@ -0,0 +1,19 @@
+/**
+ * student
+ * this is used to make the student object
+ */
+public class student {
+    private int id;
+    private String name;
+    private int fees;
+    private int grade;
+    private int feespaid;
+    private int feestotal;
+ 
+    public student(int id, string name,int grade){
+        this.id=id;
+        this.name=name;
+        this.grade=grade;
+        
+    }
+}
