0d33f6406bc9085e3c44b8869b622f18f722452c
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-06-24 14:11:03 +0530

    init teachres class

--- student.java
+++ student.java
@@ -10,10 +10,25 @@ public class student {
     private int feespaid;
     private int feestotal;
  
-    public student(int id, string name,int grade){
+    // this is constructor filling students information
+    public student(int id, String name,int grade){
+
+        this.feespaid=0;
+        this.feestotal=30000;
         this.id=id;
         this.name=name;
         this.grade=grade;
         
+    }
+
+    // function to to set the grade of the student
+    public void setGrade(int grade) {
+        this.grade=grade;
+        
+    }
+
+    //function to set the fees
+    public void updateFees(int fees) {
+        feespaid=feespaid+fees;
     }
 }
---
+++ teacher.java
@@ -0,0 +1,28 @@
+/**
+ * teacher
+ */
+public class teacher {
+    private String name;
+    private int salary;
+    private int id;
+
+
+    public teacher(int id ,String name,int salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getSalary(){
+        return this.salary;    
+    }
+
+    public int getid(){
+        return id;
+    }
+}
